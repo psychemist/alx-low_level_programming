c50d993d80bb43a2dfb2d872b95b1266719b6157
psych0wh1z <ikeagudike@gmail.com>
2022-09-09 11:16:22 +0100

    Tweak variable identifier formats

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long x;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(x));
-	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
