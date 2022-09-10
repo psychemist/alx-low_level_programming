d5f87bf4c187e092815e416b9ce7bba2574079b7
psych0wh1z <ikeagudike@gmail.com>
2022-09-10 02:34:14 +0100

    Add second task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int lastDigit;
+	char str[14] = "Last digit of";
+	char statement1[] = "and is greater than 5";
+	char statement2[] = "and is less than 6 and not 0";
+	char statement3[] = "and is 0";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
+	if (lastDigit > 5)
+	{
+		printf("%s %d is %d %s\n", str, n, lastDigit, statement1);
+	}
+	else if (lastDigit < 5)
+	{
+		printf("%s %d is %d %s\n", str, n, lastDigit, statement2);
+	}
+	else
+	{
+		printf("%s %d is %d %s", str, n, lastDigit, statement3);
+	}
+	return (0);
+}
