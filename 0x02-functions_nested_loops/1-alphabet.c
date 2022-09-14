c6a7e3c4b2065f74edebf46daf7df92ff6969d53
psych0wh1z <ikeagudike@gmail.com>
2022-09-14 04:31:31 +0100

    Add second task files

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print lowercase alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
