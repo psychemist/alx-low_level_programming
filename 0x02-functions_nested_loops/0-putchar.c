6a8ea118d197f9e0e4fb3a06628167e95eae2041
psych0wh1z <ikeagudike@gmail.com>
2022-09-14 03:35:47 +0100

    Add first solution

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints putchar, then a new line
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	printf("_putchar\n");
+	return (0);
+}
