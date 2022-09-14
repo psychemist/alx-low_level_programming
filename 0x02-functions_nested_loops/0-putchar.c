29f8e124f7adf4d00e8dd14307b5211f6504d3bb
psych0wh1z <ikeagudike@gmail.com>
2022-09-14 04:18:33 +0100

    Add header and function prototypes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  * Description: Prints putchar, then a new line
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	printf("_putchar\n");
+	char c[9] = "_putchar";
+	int i = 0;
+
+	while (i < 9)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
