9316ca0682db1611ccbacf83d8fc681a40c92b05
psych0wh1z <ikeagudike@gmail.com>
2022-09-14 05:21:07 +0100

    Add eleventh task files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - add two integers and return the result
+ *
+ * @i: First integer
+ * @j: second integer
+ *
+ * Return: (0)
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
