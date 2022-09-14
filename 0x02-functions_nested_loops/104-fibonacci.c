0cd448ad840117706c511f2bfd8d61b68b95e5b6
psych0wh1z <ikeagudike@gmail.com>
2022-09-14 11:06:08 +0100

    Solve for overflow

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Description: computes and prints the sum of all the multiples of 3 or
- * 5 below 1024 (excluded), followed by a new line
+ * main - print first 98 Fibonacci numbers without using long long, malloc,
+ * pointers, array/tables, or structures
  * Return: 0
  */
+
 int main(void)
 {
-	int i = 0;
-	unsigned long int a = 0, b = 1, next = 0;
+	int counter, overflow;
+	unsigned long a = 1;
+	unsigned long b = 1;
+	unsigned long sum = 0;
+	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
 
-	while (i < 98)
+	printf("1");
+
+	for (counter = 2; counter < 93; counter++) /* will still print */
 	{
-		next = a + b;
+		sum = a + b;
 		a = b;
-		b = next;
-		printf("%lu", next);
-
-		if (i < 97)
-		{
-			printf(", ");
-		}
-		i++;
+		b = sum;
+		printf(", %lu", sum);
 	}
-	putchar('\n');
+
+	a_head = a / 1000000000; /* break larger num into 2 parts */
+	a_tail = a % 1000000000;
+	b_head = b / 1000000000;
+	b_tail = b % 1000000000;
+
+	for (; counter < 99; counter++)
+	{
+		overflow = (a_tail + b_tail) / 1000000000;
+		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
+		sum_head = (a_head + b_head) + overflow;
+
+		printf(", %lu%lu", sum_head, sum_tail);
+
+		a_head = b_head;
+		a_tail = b_tail;
+		b_head = sum_head;
+		b_tail = sum_tail;
+	}
+
+
+	printf("\n");
+
 	return (0);
 }
