e3726ebd39173e7c99bb63dfc07eb6fd32c235d2
psych0wh1z <ikeagudike@gmail.com>
2022-09-14 04:20:37 +0100

    Correct output

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	char c[9] = "_putchar";
-	int i = 0;
-
-	while (i < 9)
-	{
-		_putchar(c[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
