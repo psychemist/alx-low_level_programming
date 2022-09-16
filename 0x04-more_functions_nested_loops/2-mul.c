8f3d0533cddc096dfe1eb9ca8749db9ca137edd0
psych0wh1z <ikeagudike@gmail.com>
2022-09-16 02:51:51 +0100

    Add Task 2 files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Receives two integers and return their product
+ * @a: First integer
+ * @b: Second integer
+ * Return: a * b
+ */
+
+int mul (int a, int b)
+{
+	return (a * b);
+}
