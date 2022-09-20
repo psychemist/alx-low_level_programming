99dfb5ab7df48ce41e69ecd0b11eb2cbc1f0d7ca
psych0wh1z <ikeagudike@gmail.com>
2022-09-20 05:06:18 +0100

    Add Task 4 files

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - Reverse a string
+ *
+ * @s: Pointer to an int
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
