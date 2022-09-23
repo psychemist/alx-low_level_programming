d9e68ebfdc268ea4078eea526ac1efc162978eea
psych0wh1z <ikeagudike@gmail.com>
2022-09-23 05:11:52 +0100

    Add Task 0 files

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAINDOTH
+#define MAINDOTH
+char *_strcat(char *dest, char *str);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *let(char *);
+#endif
