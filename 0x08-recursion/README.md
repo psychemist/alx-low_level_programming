834b4dc49e2aac6a7b992b1d141459967aee47ea
psych0wh1z <ikeagudike@gmail.com>
2022-09-29 05:36:13 +0100

    Add Task 0 files

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This is my repo on Recursion in C
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int square(int n, int val);
+int _sqrt_recursion(int n);
+int check_prime(int n, int resp);
+int is_prime_number(int n);
+
+#endif
