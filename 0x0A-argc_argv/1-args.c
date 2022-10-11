8e1e233c672b1dc9d8e9533c9cc016a126c5cdbd
psych0wh1z <ikeagudike@gmail.com>
2022-10-11 18:43:54 +0100

    Add Task 1 files

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints the name of the program
+ * @argc: number of command line arguments
+ * @argv: array of size argc
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	// (void)argv -- also used to ignore unused variables
+	printf("%d\n", argc);
+	return (0);
+}
