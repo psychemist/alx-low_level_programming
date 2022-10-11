6b31c980071d9de0717ce95750235c8e75850c9a
psych0wh1z <ikeagudike@gmail.com>
2022-10-11 15:59:36 +0100

    Add Task 0 files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry function
+ * Description: Prints the name of the program
+ * Return: 0 (Success)
+ */
+int main(int argc __attribute__ ((unused)), char *argv[])
+{
+	printf("%s", argv[0]);
+
+	return 0;
+}
