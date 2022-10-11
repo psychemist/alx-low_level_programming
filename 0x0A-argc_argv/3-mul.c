e9867ff258fb9bc741ed9bb58a29388cb0c850f4
psych0wh1z <ikeagudike@gmail.com>
2022-10-11 19:37:59 +0100

    Add Task 3 files

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the name of the program
+ * @argc: number of command line arguments
+ * @argv: array of size argc
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+	int arg1, arg2;
+
+	if (argc !=3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+		arg1 = atoi(argv[1]);
+		arg2 = atoi(argv[2]);
+		mul = arg1 * arg2;
+		printf("%d\n", mul);
+	}
+		return (0);
+}
