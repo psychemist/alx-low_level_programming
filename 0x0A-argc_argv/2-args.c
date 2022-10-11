793921cbf68ccb428b39d1701eb5789e2349aa46
psych0wh1z <ikeagudike@gmail.com>
2022-10-11 19:11:51 +0100

    Add Task 2 files

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the name of the program
+ * @argc: number of command line arguments
+ * @argv: array of size argc
+ * Return: 0 (Success)
+ */
+int main(int argc, char * argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	
+	return (0);
+}
