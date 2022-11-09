a9f176ce61d19292701f5133c6fd2c10550776a3
psych0wh1z <ikeagudike@gmail.com>
2022-11-09 15:28:58 +0100

    Remove casts from malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,12 +13,12 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	mesh = (int **)malloc(sizeof(int *) * height);
+	mesh = malloc(sizeof(int *) * height);
 	if (mesh == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		mesh[i] = (int *)malloc(sizeof(int) * width);
+		mesh[i] = malloc(sizeof(int) * width);
 		if (mesh[i] == NULL)
 		{
 			for (i = 0; i < height; i++)
