2ca1e51808f081a02cc7964d2a2fc507f8a3f4c2
psych0wh1z <ikeagudike@gmail.com>
2022-11-09 21:15:47 +0100

    Add task0 files

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * malloc_checked - Allocates memory using malloc
+  * @b: size of r+w memory space to be allocated in heap
+  * Return: Pointer to allocated memory
+  */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
