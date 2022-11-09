46bac1a69cde8df14af556c78a430c587b2561a8
psych0wh1z <ikeagudike@gmail.com>
2022-11-09 19:55:13 +0100

    Fix string not returning new lines

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,40 +1,57 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * _strlen - Calculates the length of a string
+ * @s: Pointer to a char
+ * Return: Integer value that is the ength of passed string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
  * argstostr - Concatenates all the arguments of the program
  * @ac: argc of main program
  * @av: argv of main program
  * Return: Pointer to new string
  */
+
 char *argstostr(int ac, char **av)
 {
 	int i, j;
-	int x = 0, len = 0;
+	int k = 0, len = 0;
 	char *args;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+
+	/* find length of malloc */
 	for (i = 1; i < ac; i++)
-	{
-		j = 0;
-		while (av[i][j])
-		{
-			len++;
-			j++;
-		}
-	}
-	len++;
-	args = malloc(len + (ac - 1));
+		len += _strlen(av[i]);
+
+	/* add space for new lines and null terminator */
+	len += ac;
+	args = malloc(len * sizeof(char));
 	if (args == NULL)
 		return (NULL);
+
+	/* assign each argument into new array */
 	for (i = 1; i < ac; i++)
 	{
-		for (j = 0; av[i][j]; j++)
+		for (j = 0; j < _strlen(av[i]); j++)
 		{
-			args[x] = av[i][j];
-			x++;
+			args[k] = av[i][j];
+			k++;
 		}
-		args[x] = '\n';
+		args[k++] = '\n';
 	}
 	args[len] = '\0';
 	return (args);
