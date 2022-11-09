265220e1701036220ddb4fbcd758697f57044b6b
psych0wh1z <ikeagudike@gmail.com>
2022-11-09 12:23:33 +0100

    Fix NULL overflow error

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,23 +9,20 @@
 int **alloc_grid(int width, int height)
 {
 	int **mesh;
-	int i, j, x;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	mesh = (int **)malloc(sizeof(int *) * height);
-	for (x = 0; x < height; x++)
-		mesh[x] = (int *)malloc(sizeof(int) * width);
 	if (mesh == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		j = 0;
-		while (j < width)
-		{
+		mesh[i] = (int *)malloc(sizeof(int) * width);
+		if (mesh[i] == NULL)
+			return (NULL);
+		for (j = 0; j < width; j++)
 			*(*(mesh + i) + j) = 0;
-			j++;
-		}
 	}
 	return (mesh);
 }
