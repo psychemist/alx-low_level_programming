8e1cf992e205d78b714bc6a5677cef18f9d6efb9
psych0wh1z <ikeagudike@gmail.com>
2022-11-09 11:47:43 +0100

    Add task3 files

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - Creates a 2D grid of integers
+ * @width: number of columns of the array
+ * @height: number of rows of the array
+ * Return: Pointer to a multi-dimensional 2D array of integers
+ */
+int **alloc_grid(int width, int height)
+{
+	int **mesh;
+	int i, j, x;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	mesh = (int **)malloc(sizeof(int *) * height);
+	for (x = 0; x < height; x++)
+		mesh[x] = (int *)malloc(sizeof(int) * width);
+
+
+	for (i = 0; i < height; i++)
+	{
+		j = 0;
+		while (j < width)
+		{
+			*(*(mesh + i) + j) = 0;
+			j++;
+		}
+	}
+	return (mesh);
+}
