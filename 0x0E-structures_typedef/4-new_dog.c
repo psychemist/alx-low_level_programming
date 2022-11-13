59502fade2eca2fb1171f7b1e4474521ef54ba43
psych0wh1z <ikeagudike@gmail.com>
2022-11-13 07:52:05 +0100

    Fix Betty code style

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -30,7 +30,7 @@ char *_strcpy(char *dest, char *src)
 
 	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
-        dest[i] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
@@ -53,7 +53,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (dog == NULL)
 		return (NULL);
-	
+
 	dog->age = age;
 
 	name_copy = malloc(_strlen(name) + 1);
