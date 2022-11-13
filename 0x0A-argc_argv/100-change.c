4db184d25cd5c9b117b1605a4b69cc4be313069d
psych0wh1z <ikeagudike@gmail.com>
2022-11-13 11:02:16 +0100

    Add advanced task file

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - calculates the minimum number of coins to make change for an amount
+ * @argc: number of program arguments
+ * @argv: array of program arguments
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int num, div = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num = atoi(argv[1]);
+
+	if (num < 0)
+		printf("%d\n", 0);
+
+	if (num >= 25)
+	{
+		div += num / 25;
+		num %= 25;
+	}
+	if (num >= 10 && num <= 24)
+	{
+		div += num / 10;
+		num %= 10;
+	}
+	if (num >= 5 && num <= 9)
+	{
+		div += num / 5;
+		num %= 5;
+	}
+	if (num >= 2 && num <= 4)
+	{
+		div += num / 2;
+		num %= 2;
+	}
+	if (num >= 0 && num <= 1)
+	{
+		div += num / 1;
+	}
+	printf("%d\n", div);
+	return (0);
+}
