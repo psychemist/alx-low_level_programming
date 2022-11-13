d6c6e8dfe3fd44b1e7de2dfde541ab1c2e1f0c0d
psych0wh1z <ikeagudike@gmail.com>
2022-11-13 09:19:21 +0100

    Fix integer checking function

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * check_int - checks if a string is a valid number
+ * @args: array of string sto be checked
+ * Return: 0 or 1
+ */
+
+int check_int(char *args)
+{
+	int i, check = 0;
+
+	for (i = 0; args[i] != '\0'; i++)
+	{
+		if (args[i] >= '0' && args[i] <= '9')
+		{
+			check = 1;
+		}
+		else
+		{
+			check = 0;
+			return (check);
+		}
+	}
+
+	return (check);
+}
+
+/**
  * main - Prints the name of the program
  * @argc: number of command line arguments
  * @argv: array of size argc
  * Return: 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, sum = 0;
 
 	if (!(argc > 1))
 	{
@@ -18,7 +45,15 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			sum += atoi(argv[i]);
+			if (check_int(argv[i]))
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		printf("%d\n", sum);
 	}
