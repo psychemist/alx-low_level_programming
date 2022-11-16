6836833f5215a8039df2eddb091e9fbe8aebd777
psych0wh1z <ikeagudike@gmail.com>
2022-11-16 10:27:58 +0100

    Fix betty warnings

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
 #include "function_pointers.h"
 /**
- * 
- * 
- * 
- * 
- * 
+ * print_name - accepts a callback function tha prints a name
+ * @name: string that is the name to be printed
+ * @f: function to a pointer that accepts an array of chars and returns nothing
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
