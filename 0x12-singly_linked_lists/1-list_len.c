32fcc973a602bf0758c1d8338be8061643cb73b5
psych0wh1z <ikeagudike@gmail.com>
2022-11-21 08:34:34 +0100

    Change counter conditional statement

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,16 +8,11 @@ size_t list_len(const list_t *h)
 {
 	unsigned int count = 0;
 
-	if (h != NULL)
+	while (h != NULL)
 	{
-		while (h->next != NULL)
-		{
-			if (h->str != NULL)
-				++count;
-			if (h->len)
-				++count;
-			h = h->next;
-		}
+		if (h->str != NULL && h->len != 0)
+			count =+ 2;
+		h = h->next;
 	}
 
 	return (count);
