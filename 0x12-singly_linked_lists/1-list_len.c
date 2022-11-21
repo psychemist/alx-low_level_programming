5be098a42623b1fd5b7aef1585e31a26b81367b3
psych0wh1z <ikeagudike@gmail.com>
2022-11-21 08:22:25 +0100

    Add task1 files

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,12 +8,17 @@ size_t list_len(const list_t *h)
 {
 	unsigned int count = 0;
 
-	if (h == NULL)
-		return (0);
-	if (h->str)
-		++count;
-	if (h->len)
-		++count;;
+	if (h != NULL)
+	{
+		do {
+			if (h->str != NULL)
+				++count;
+			if (h->len)
+				++count;
+			h = h->next;
+		} while (h->next != NULL);
+	}
+
 	return (count);
 }
 
