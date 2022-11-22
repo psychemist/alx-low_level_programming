e76b3e23008a0c501d921c4d51b6d3384e83ecb3
psych0wh1z <ikeagudike@gmail.com>
2022-11-22 06:26:23 +0100

    Handle malloc return

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -27,17 +27,18 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new = malloc(sizeof(list_t));
 
 	if (new == NULL)
-		free(new);
+		return (NULL);
+
+	if (str == NULL)
+		return (NULL);
 
 	new->str = strdup(str);
 	if (new->str == NULL)
-	{
-		free(new->str);
-		free(new);
-	}
+		return (NULL);
+
 	new->len = _strlen(str);
 	new->next = *head;
 	*head = new;
 
-	return (*head);
+	return (new);
 }
