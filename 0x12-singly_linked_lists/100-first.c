8e4a4ae64d49508d2aea979500b80a62d63996c5
psych0wh1z <ikeagudike@gmail.com>
2022-11-22 09:38:43 +0100

    add first advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * 
+ * 
+ */
+void tortue(void) __attribute__ ((constructor));
+void tortue(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
