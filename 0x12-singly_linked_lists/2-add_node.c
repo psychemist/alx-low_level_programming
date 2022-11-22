ade74520c708a2878055a3a57931f1883bcf2e63
psych0wh1z <ikeagudike@gmail.com>
2022-11-22 06:14:08 +0100

    Add task2 files

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strlen - returns the length of the string that was passed
+ * @s: pointer to a char
+ * Return: integer (length of string)
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * add_node - adds a new node to the beginning of a linked list
+ * @head: head node of the list
+ * @str: pointer to a char [array]
+ * Return: pointer to a list_t struct
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		free(new);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new->str);
+		free(new);
+	}
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
