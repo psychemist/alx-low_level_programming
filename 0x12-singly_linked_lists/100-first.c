712cd4dca3415a483a136ff33680dfd66cc322b8
psych0wh1z <ikeagudike@gmail.com>
2022-11-22 09:56:13 +0100

    Fix attribute declaration

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
  * tortue - prints a string before the main function is executed
+ * Note: made possible by prior declaration of "__attribute__ ((constructor))"
  * Return: nothing
  */
-void tortue(void) __attribute__ ((constructor));
-void tortue(void)
+void __attribute__ ((constructor)) tortue(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
