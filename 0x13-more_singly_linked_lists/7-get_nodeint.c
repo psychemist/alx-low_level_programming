730c2a215dece7baa0ca96f3aa6fa7c066294581
psych0wh1z <ikeagudike@gmail.com>
2022-11-23 04:13:05 +0100

    Fix return value

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,7 +7,6 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *node;
 	unsigned int i = 0;
 
 	if (head == NULL)
@@ -20,8 +19,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	}
 
 	if (i == index)
-		node = head;
+		return (head);
 
-	return (node);
+	return (NULL);
 }
 
