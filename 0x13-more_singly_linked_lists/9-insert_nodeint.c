78d0d78ee7d810b3699924668ac42bdf0a6f39d4
psych0wh1z <ikeagudike@gmail.com>
2022-11-23 05:35:54 +0100

    Remove NULL clause

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,8 +11,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i, index = idx - 1;
 	listint_t *node, *last;
 
-	if (*head == NULL && idx == 0)
-		add_nodeint_end(head, n);
+/*
+   	if (*head == NULL &&|| idx == 0)
+	{
+		head->n = n;
+		head->next = NULL;
+	}
+*/
 
 	last = *head;
 
