fe9beeae351b01302397df4884ab9a1c7cfd5015
psych0wh1z <ikeagudike@gmail.com>
2022-11-25 17:22:31 +0100

    Add task3 files

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * set_bit - sets the value of a bit at a given index too 1
+ * @n: pointer to an integral number
+ * @index: index of desired bit
+ * Return: 1 (success) or -1 (error)
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	*n |= (1 << index);
+
+	return (1);
+}
+
