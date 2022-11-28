8d8c708a6eecf25ed6960cd74c7d8a9c74593d38
psych0wh1z <ikeagudike@gmail.com>
2022-11-28 06:49:16 +0100

    Set file permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/* open */
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
