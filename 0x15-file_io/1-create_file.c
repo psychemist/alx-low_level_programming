89285e7a9cfd5158e5b1c8eb866b54c63747ae0f
psych0wh1z <ikeagudike@gmail.com>
2022-11-28 06:46:57 +0100

    Fix alloc size

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,17 +17,17 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/* open */
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC);
 	if (fd == -1)
 		return (-1);
-
-	size = strlen(text_content);
 
 	if (text_content == NULL)
 		text_content = "";
 
+	size = strlen(text_content);
+
 	/* write */
-	fdd = write(fd, text_content, ++size);
+	fdd = write(fd, text_content, size);
 
 	if (fdd == -1)
 	{
