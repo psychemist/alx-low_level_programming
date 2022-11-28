9224ec02b620ead0bd150533caa90d78ce027579
psych0wh1z <ikeagudike@gmail.com>
2022-11-28 07:28:47 +0100

    Fix NULL return error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	/* get size of string */
 
 	if (text_content == NULL)
-		return (-1);
+	{
+		if (!filename)
+			return (-1);
+		else
+			return (1);
+	}
 
 	size = strlen(text_content);
 
