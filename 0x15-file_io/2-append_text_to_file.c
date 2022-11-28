59576620d22a05961400e185d08cdc151d875816
psych0wh1z <ikeagudike@gmail.com>
2022-11-28 07:24:44 +0100

    Add task2 files

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: name of file
+ * @text_content: NULL terminated string to add at the end of the file
+ * Return: 1 (success) or -1 (failure)
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, fd2, size;
+
+	if (filename == NULL)
+		return (-1);
+
+	/* open */
+
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	/* get size of string */
+
+	if (text_content == NULL)
+		return (-1);
+
+	size = strlen(text_content);
+
+	/* append */
+	fd2 = write(fd, text_content, size);
+
+	if (fd2 == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
+
