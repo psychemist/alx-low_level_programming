bcabcc23744a261f76228626e7c76ddbe0ba4866
psych0wh1z <ikeagudike@gmail.com>
2022-11-29 19:53:15 +0100

    Fix while loop

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,7 +33,7 @@ ssize_t write_file(char *arg2)
 {
 	int fdw;
 
-	fdw = open(arg2, O_RDWR | O_CREAT | O_TRUNC, 0664);
+	fdw = open(arg2, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 	if (fdw == -1)
 	{
@@ -86,9 +86,8 @@ int main(int ac, char *av[])
 
 	buff = malloc(sizeof(char) * 1025);
 
-	do
+	while ((read_t = read(fd1, buff, 1024) != 0))
 	{
-		read_t = read(fd1, buff, 1024);
 		if (read_t == -1)
 		{
 			dprintf(2, "Error: Can't read from file %s\n", av[1]);
@@ -101,9 +100,9 @@ int main(int ac, char *av[])
 		{
 			close(fd1);
 			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			exit(99);
 		}
-		fd2 = open(av[2], O_WRONLY | O_APPEND);
-	} while (read_t != -1);
+	}
 
 	free(buff);
 	close_file(fd1);
