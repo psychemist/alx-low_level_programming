8abd4ae5a9035eaf5ade3fad8260782a23ffbbd2
psych0wh1z <ikeagudike@gmail.com>
2022-11-29 20:34:47 +0100

    Clean up code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,107 +1,70 @@
 #include "main.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 /**
- * 
- * 
- * 
- */
-ssize_t read_file(char *arg1)
+ * __exit - prints error messages and exits with exit value
+ * @error: num is either exit value or file descriptor
+ * @str: pointer to string (filenamme)
+ * @fd: file descriptor
+ * Return: 0 on success
+ **/
+int __exit(int error, char *str, int fd)
 {
-	int fdr;
-	
-	fdr = open(arg1, O_RDONLY);
-
-	if (fdr == -1)
+	switch (error)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg1);
-		exit(98);
+	case 97:
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(error);
+	case 98:
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", str);
+		exit(error);
+	case 99:
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", str);
+		exit(error);
+	case 100:
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
+		exit (error);
+	default:
+		return (0);
 	}
-
-	return (fdr);
 }
-
-/**
- * 
- * 
- * 
- * 
- */
-ssize_t write_file(char *arg2)
-{
-	int fdw;
-
-	fdw = open(arg2, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	if (fdw == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg2);
-		exit(99);
-	}
-
-	return (fdw);
-}
-
-/**
- * 
- * 
- * 
- * 
- */
- void close_file(int fdesc)
- {
-	 int fdc;
-
-	 fdc = close(fdesc);
-
-	 if (fdc == -1)
-	 {
-		 dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fdesc);
-		 exit (100);
-	 }
- }
 
 /**
  * main - copies the content of a file to another file
- * @ac: number of arguments passed to main
+ * @ac: number of arguments passed to main (3)
  * @av: pointer to array of string arguments passed to main
- * Return: Always 0
+ * Return: 0 (success) or 97-100 (failure)
  */
 int main(int ac, char *av[])
 {
-	int fd1, fd2;
-	ssize_t read_t, write_t;
+	int fd_1, fd_2, read_t, write_t;
 	char *buff[1024];
 
 	if (ac != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		__exit(97, NULL, 0);
 
-	fd1 = read_file(av[1]);
-	fd2 = write_file(av[2]);
+	/*sets file descriptor for copy-from file*/
+	fd_1 = open(av[1], O_RDONLY);
+	if (fd_1 == -1)
+		__exit(98, av[1], 0);
 
-	while ((read_t = read(fd1, buff, 1024) != 0))
+	/* sets file descriptor for copy-to file */
+	fd_2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_2 == -1)
+		__exit(99, av[2], 0);
+
+	/*reads original file as long as there's more than 0 to read*/
+	/*copies/writes contents into new file */
+	while ((read_t = read(fd_1, buff, 1024)) != 0)
 	{
 		if (read_t == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
+			__exit(98, av[1], 0);
 
-		write_t = write(fd2, buff, 1024);
+		write_t = write(fd_2, buff, read_t);
 		if (write_t == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
-			exit(99);
-		}
+			__exit(99, av[2], 0);
 	}
 
-	close_file(fd1);
-	close_file(fd2);
+	close(fd_2) == -1 ? __exit(100, NULL, fd_2) : close(fd_2);
+	close(fd_1) == -1 ? __exit(100, NULL, fd_1) : close(fd_1);
 
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* open */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/* read, write, close */
+#include <unistd.h>
+
+/* dprintf */
 #include <stdio.h>
+
+/* malloc, free */
 #include <stdlib.h>
-#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
