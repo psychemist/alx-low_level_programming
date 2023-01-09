6a93dcf8baecd20eb347176313b68f3e7d217f99
psych0wh1z <ikeagudike@gmail.com>
2023-01-09 15:59:22 +0100

    Add secon advanced task files

---
+++ 0x17-doubly_linked_lists/102-palindrome.py
@@ -0,0 +1,12 @@
+palindrome = []
+count = 0
+
+for num1 in range(100, 1000):
+    for num2 in range(100, 1000):
+        product = num1 * num2
+        if str(product) == str(product)[::-1]:
+            palindrome.append(product)
+            count += 1
+#           print(f"#{count} - {num1} * {num2} = {palindrome[-1]}")
+
+print(max(palindrome))
