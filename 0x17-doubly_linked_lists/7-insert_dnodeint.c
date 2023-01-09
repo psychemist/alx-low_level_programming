f00bce77bbbfef465beda02d2f57c9c1cf15f29b
psych0wh1z <ikeagudike@gmail.com>
2023-01-09 06:53:45 +0100

    Modify node insertion code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int index = 0;
-	dlistint_t *new_node, *current;
+	dlistint_t *new_node, *temp;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -19,17 +19,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (*h == NULL)
 		return (NULL);
-	current = *h;
 
-	while (current != NULL && index < idx)
+	if (idx == 0)
 	{
-		current = current->next;
-		index++;
+		new_node->prev = NULL;
+		new_node->next = *h;
+		(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
+	else
+	{
+		temp = *h;
+		while (index < (idx - 1))
+		{
+			temp = temp->next;
+			if (temp == NULL)
+			{
+				free(new_node);
+				return (NULL);
+			}
+			index++;
+		}
 	}
 
-	new_node->prev = current;
-	new_node->next = current->next;
-	current->next = new_node;
+	new_node->prev = temp;
+	new_node->next = temp->next;
+	temp->next = new_node;
 
 	return (new_node);
 }
