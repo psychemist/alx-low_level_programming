a079eef965360ea6ccce4e6d381bd43a4168c4ab
psych0wh1z <ikeagudike@gmail.com>
2023-01-09 16:13:53 +0100

    Fix typo

--- 0x17-doubly_linked_lists/102-palindrome.py
+++ 0x17-doubly_linked_lists/102-palindrome.py
@@ -4,9 +4,9 @@ count = 0
 for num1 in range(100, 1000):
     for num2 in range(100, 1000):
         product = num1 * num2
-        if str(product) == str(product)[::-1]:
+        if num2 > num1 and str(product) == str(product)[::-1]:
             palindrome.append(product)
             count += 1
-#           print(f"#{count} - {num1} * {num2} = {palindrome[-1]}")
+            print(f"#{count} - {num1} * {num2} = {palindrome[-1]}")
 
 print(max(palindrome))
