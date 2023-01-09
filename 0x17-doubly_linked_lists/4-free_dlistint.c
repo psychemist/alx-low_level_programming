483051553fa8ebaa46c87e3e090171bc5c5c3530
psych0wh1z <ikeagudike@gmail.com>
2023-01-09 05:53:28 +0100

    Fix betty errors

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 /**
- * free_dlistint - 
- * @head: pointer to first node of doubly linked list
+ * free_dlistint -  frees a doubly linked list
+ * @head: pointer to first node of linked list
  * Return: void
  */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *node;
-	
+
 	while (head != NULL)
 	{
 		node = head;
