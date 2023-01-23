75b34725d3eae95b923ac92f75ea2929ad8f7d12
psych0wh1z <ikeagudike@gmail.com>
2023-01-23 01:58:55 +0100

    Add task0 files

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table (SUCCESS) or NULL (FAIL)
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = calloc(table->size, sizeof(hash_node_t));
+
+	if (table->array == NULL)
+		return (NULL);
+
+	return (table);
+}
