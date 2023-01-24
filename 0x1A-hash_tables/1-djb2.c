351705df22d00af6f39154f5030d6f527fe9baec
psych0wh1z <ikeagudike@gmail.com>
2023-01-24 09:49:04 +0100

    Add source link

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,5 +1,6 @@
 /**
  * hash_djb2 - implementation of the djb2 algorithm
+ * source: https://gist.github.com/papamuziko/7bb52dfbb859fdffc4bd0f95b76f71e8
  * @str: string used to generate hash value
  *
  * Return: hash value
