0b749f4f710dff151f1d36ae007ee643f82dd3bd
psych0wh1z <ikeagudike@gmail.com>
2023-01-24 10:46:40 +0100

    Add task4 files

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key in a hash table
+ * @ht: hash table of saved key/value pairs
+ * @key: key to a saved value
+ * Return: value of node (SUCCESS) or NULL (FAIL)
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx;
+	hash_node_t *node;
+
+	idx = key_index((const unsigned char *)key, ht->size);
+
+	node = ht->array[idx];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
