90a126298e70d8d606b196b63a7d5c5f3f80ac97
psych0wh1z <ikeagudike@gmail.com>
2023-01-24 08:16:31 +0100

    Fix hash table creation and updating

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,30 +1,28 @@
 #include "hash_tables.h"
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: hash table to add or update the key/value to
- * @key: key (can not be an empty string)
- * @value: value associated with key
+ * create_update_node - creates or updates a new or existing hash table node
+ * @ht: a hash table implemented with separate chaining collision handling
+ * @key: key at index of hash table
+ * @value: value of key
+ * @index: index of node in hash table array
  * Return: 1 (SUCCESS) or 0 (FAIL)
  */
-
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+int create_update_node(hash_table_t *ht, const char *key, const char *value,
+		unsigned long int index)
 {
-	int idx;
 	char *k, *v;
-	hash_node_t *node;
+	hash_node_t *node, *current;
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
 
-	if (key != NULL)
-		k = strdup(key);
+	k = strdup(key);
 	if (!k)
 	{
 		free(node);
 		return (0);
 	}
-
 	v = strdup(value);
 	if (!v)
 	{
@@ -35,23 +33,41 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	node->key = k;
 	node->value = v;
-	node->next = NULL;
 
-	idx = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
 
-	if (ht->array[idx] == NULL)
+	if (current == NULL)
 	{
-		ht->array[idx] = node;
+		node->next = NULL;
+		current = node;
 	}
-	else if (ht->array[idx]->key == key)
-	{
-		ht->array[idx]->value = v;
-	}
+	else if (strcmp(current->key, key) == 0)
+		current->value = v;
 	else
 	{
-		node->next = ht->array[idx];
-		ht->array[idx] = node;
+		node->next = ht->array[index];
+		current = node;
 	}
 
 	return (1);
+}
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table to add or update the key/value to
+ * @key: key (can not be an empty string)
+ * @value: value associated with key
+ * Return: 1 (SUCCESS) or 0 (FAIL)
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+
+	if (ht == NULL || key == NULL)
+		return (0);
+
+	idx = key_index((unsigned char *)key, ht->size);
+
+	return (create_update_node(ht, key, value, idx));
 }
