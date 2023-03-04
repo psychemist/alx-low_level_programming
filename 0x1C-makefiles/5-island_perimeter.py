834d15f980ad68e7a09c00e60a1835fdb84a9ee7
psych0wh1z <ikeagudike@gmail.com>
2023-03-04 15:51:50 +0100

    Add task5 file

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+5-island perimeter module calculates perimeter of an island
+"""
+
+
+def island_perimeter(grid):
+    """
+    Returns:
+        (int): perimeter of the island described in grid
+
+    Args:
+        grid (matrix): list of list of integers
+    """
+    rows = len(grid)
+    columns = len(grid[0])
+    count = 0
+
+    for line in grid:
+        for num in line:
+            if num == 1:
+                count += 4
+
+    for row in range(rows):
+        for col in range(columns):
+            if grid[row][col] == 1:
+                if grid[row][col - 1] == 1:
+                    count -= 2
+                if grid[row - 1][col] == 1:
+                    count -= 2
+
+    return count
