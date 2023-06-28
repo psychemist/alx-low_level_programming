06dc2a36698d3520f5501e115ca6b19c7e44c9bc
psych0wh1z <ikeagudike@gmail.com>
2023-06-28 23:19:52 +0100

    Add own solution

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -32,3 +32,22 @@ int main(void)
 	}
 	return (0);
 }
+int main(void)
+{
+        int n, x;
+        char str1[] = "Last digit of";
+        char str2[] = "greater than 5";
+        char str3[] = "less than 6 and not 0";
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        x = n % 10;
+
+        if (x > 5)
+                printf("%s %i is %d and is %s\n", str1, n, x, str2);
+        else if (x < 5)
+                printf("%s %i is %d and is %s\n", str1, n, x, str3);
+        else
+                printf("%s %i is %d and is 0\n", str1, n, x);
+        return (0);
+}
